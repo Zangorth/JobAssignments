ba19d8d245619410e61da08dfb8b95c2cdcd687f
Zangorth <Samuel.A.York.1@gmail.com>
2021-08-26 00:22:31 -0400

    Removing Files

--- DriveTime/streamlit_app.py
+++ DriveTime/streamlit_app.py
@@ -1,17 +1,23 @@
 import streamlit as st
 import sys
 
+# Update to web version after presentation
+
 sys.path.append(r'C:\Users\Samuel\Google Drive\Portfolio\Jobs\DriveTime')
 import streamlit_helper
 
+st.set_page_config(layout='wide')
 st.sidebar.header('Navigation')
-page = st.sidebar.selectbox('', ['Model', 'Plots', 'Worst Delinquency', 'Pipeline'])
+page = st.sidebar.selectbox('', ['Model', 'Plots', 'Summary', 'Worst Delinquency', 'Pipeline'])
 
 if page == 'Model':
     streamlit_helper.model()
 
 elif page == 'Plots':
     streamlit_helper.plots()
+    
+elif page == 'Summary':
+    streamlit_helper.summary()
 
 elif page == 'Worst Delinquency':
     streamlit_helper.worst_delinquency()
